497645cca1339255ebb16d65b9597e0c2778d2a1
wblm <wblm@LAPTOP-RHSPI2BT.mshome.net>
2022-10-13 16:07:55 +0800

    2020-01-01

--- Main/src/myobj/Main.java
+++ Main/src/myobj/Main.java
@@ -6,166 +6,233 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
-		int n = cin.nextInt();
-		int m = cin.nextInt();
-		int q = cin.nextInt();
-		ArrayList<Role> roleList = new ArrayList<Role>();
-		ArrayList<RoleRelevance> roleRelevanceList = new ArrayList<RoleRelevance>();
-		User user = new User();
-		for(int i = 0; i < n; ++i) {
-			Role role = new Role();
-			role.setName(cin.next());
-			int nv = cin.nextInt();
-			for(int j = 0; j < nv; ++j) {
-				role.getOperationList().add(cin.next());
-			}
-			int no = cin.nextInt();
-			for(int j = 0; j < no; ++j) {
-				role.getResourceKindList().add(cin.next());
-			}
-			int nn = cin.nextInt();
-			for(int j = 0; j < nn; ++j) {
-				role.getResourceNameList().add(cin.next());
-			}
-			roleList.add(role);
-		}
-		for(int i = 0; i < m; ++i) {
-			RoleRelevance roleRelevance = new RoleRelevance();
-			roleRelevance.setRoleName(cin.next());
-			int ns = cin.nextInt();
-			for(int j = 0; j < ns; ++j) {
-				String firstStr = cin.next();
-				String nextStr = cin.next();
-				if("u".equals(firstStr)) {
-					roleRelevance.getAuthorizationObjectName().add(nextStr);
+		int t = cin.nextInt();
+		cin.nextLine();
+		FileRoot system = new FileRoot();
+		while(t-- != 0) {
+			String s = cin.nextLine();
+			String[] operator = s.split(" ");
+			if("C".equals(operator[0])) {
+				if(system.createFile(operator[1].split("/"), Long.parseLong(operator[2]))) {
+					System.out.println("Y");
 				}
-				else if("g".equals(firstStr)) {
-					roleRelevance.getAuthorizationObjectNameGroup().add(nextStr);
+				else {
+					System.out.println("N");
 				}
 			}
-			roleRelevanceList.add(roleRelevance);
-		}
-		for(int i = 0; i < q; ++i) {
-			user.setName(cin.next());
-			int ng = cin.nextInt();
-			for(int j = 0; j < ng; ++j) {
-				user.getUserGroup().add(cin.next());
+			else if("R".equals(operator[0])) {
+				system.removeFile(operator[1].split("/"));
+				System.out.println("Y");
 			}
-			String operator = cin.next();
-			String resourceKind = cin.next();
-			String resourceName = cin.next();
-			if(user.canDoOperation(roleList,
-					user.getRelevanceRole(roleRelevanceList), operator, resourceKind, resourceName)) {
-				System.out.println(1);
-			}
-			else {
-				System.out.println(0);
+			else if("Q".equals(operator[0])) {
+				if(system.setQuota(operator[1].split("/"), Long.parseLong(operator[2]),
+						Long.parseLong(operator[3]))) {
+					System.out.println("Y");
+				}
+				else {
+					System.out.println("N");
+				}
 			}
 		}
 		cin.close();
 	}	
 };
 
-class User{
-	private String name;
-	private ArrayList<String> userGroup;
-	User(){
-		userGroup = new ArrayList<String>();
+class FileNode{
+	boolean type;
+	long size;
+	long catalogQuota, usedCatalogQuota;
+	long unbornQuota, usedUnbornQuota;
+	HashMap<String, FileNode> unborn;
+	FileNode() {
+		this.usedCatalogQuota = 0;
+		this.usedUnbornQuota = 0;
+		this.catalogQuota = 0;
+		this.unbornQuota = 0;
+		this.size = 0;
+		this.type = false;
+		this.unborn = new HashMap<>();
 	}
-	public String getName() {
-		return name;
+	FileNode(long size) {
+		this.usedCatalogQuota = 0;
+		this.usedUnbornQuota = 0;
+		this.catalogQuota = 0;
+		this.unbornQuota = 0;
+		this.type = true;
+		this.size = size;
+		this.unborn = new HashMap<>();
 	}
-	public ArrayList<String> getRelevanceRole(ArrayList<RoleRelevance> roleRelevanceList) {
-		ArrayList<String> relevanceRoleList = new ArrayList<String>();
-		for(RoleRelevance roleRelevance : roleRelevanceList) {
-			if(roleRelevance.getAuthorizationObjectName().contains(name)) {
-				relevanceRoleList.add(roleRelevance.getRoleName());
-				continue;
+ }
+class FileRoot{
+	FileNode root;
+	FileRoot(){
+		root = new FileNode();
+	}
+	public boolean createFile(String[] path, long size) {
+		if(path.length == 0) {
+			return false;
+		}
+		FileNode nowNode = root;
+		int i;
+		long beReplaceSize = size - isReplace(path);
+		for(i = 1; i < path.length - 1; ++i) {
+			if(nowNode.unborn.containsKey(path[i])) {
+				if(nowNode.unborn.get(path[i]).type == true) {
+					return false;
+				}
+				else {
+					if(checkUnbornQuota(nowNode, beReplaceSize)){
+						nowNode = nowNode.unborn.get(path[i]);
+					}
+					else {
+						return false;
+					}
+				}
 			}
-			for(String strUserGroup : userGroup) {
-				if(roleRelevance.getAuthorizationObjectNameGroup().contains(strUserGroup)) {
-					relevanceRoleList.add(roleRelevance.getRoleName());
-					break;
+			else {
+				if(checkUnbornQuota(nowNode, size)) {
+					nowNode.unborn.put(path[i], new FileNode());
+					nowNode = nowNode.unborn.get(path[i]);
+				}
+				else {
+					return false;
 				}
 			}
 		}
-		return relevanceRoleList;
-	}
-	public boolean canDoOperation(ArrayList<Role> roleList, ArrayList<String> relevanceRoleList,
-			String operation, String resourceKind, String resourceName) {
-		for(Role role : roleList) {
-			for(String relevance : relevanceRoleList) {
-				if(role.getName().equals(relevance) && 
-						role.canDoOperation(operation, resourceKind, resourceName)) {
-					return true;
+		if(!nowNode.unborn.containsKey(path[i])) {
+			if(checkQuota(nowNode, size) && checkUnbornQuota(nowNode, size)) {
+				nowNode.unborn.put(path[i], new FileNode(size));
+				nowNode.usedCatalogQuota += size;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			if(nowNode.unborn.get(path[i]).type == false) {
+				return false;
+			}
+			else {
+				if(canReplace(nowNode, nowNode.unborn.get(path[i]), size)) {
+					nowNode.usedCatalogQuota += beReplaceSize;
+					nowNode.unborn.get(path[i]).size = size;
+				}
+				else {
+					return false;
 				}
 			}
 		}
-		return false;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public ArrayList<String> getUserGroup(){
-		return userGroup;
-	}
-}
-class Role{
-	private String name;
-	private ArrayList<String> operationList;
-	private ArrayList<String> resourceKindList;
-	private ArrayList<String> resourceNameList;
-	Role(){
-		operationList = new ArrayList<String>();
-		resourceKindList = new ArrayList<String>();
-		resourceNameList = new ArrayList<String>();
-	}
-	public boolean canDoOperation(String operation, String resourceKind, String resourceName) {
-		if(!operationList.contains(operation) && !operationList.contains("*")) {
-			return false;
-		}
-		if(!resourceKindList.contains(resourceKind) && !resourceKindList.contains("*")) {
-			return false;
-		}
-		if(!resourceNameList.contains(resourceName) && !resourceNameList.isEmpty()) {
-			return false;
+		nowNode = root;
+		for(i = 1; i < path.length; ++i) {
+			nowNode.usedUnbornQuota += beReplaceSize;
+			nowNode = nowNode.unborn.get(path[i]);
 		}
 		return true;
 	}
-	public String getName() {
-		return name;
+	public boolean removeFile(String[] path) {
+		if(path.length == 0) {
+			return true;
+		}
+		FileNode nowNode = root;
+		int i;
+		long toBeDeleteSize = 0;
+		for(i = 1; i < path.length - 1; ++i) {
+			if(nowNode.unborn.containsKey(path[i])) {
+				nowNode = nowNode.unborn.get(path[i]);
+			}
+			else {
+				return true;
+			}
+		}
+		if(nowNode.unborn == null) {
+			return true;
+		}
+		if(nowNode.unborn.containsKey(path[i])) {
+			if(nowNode.unborn.get(path[i]).type) {
+				toBeDeleteSize = nowNode.unborn.get(path[i]).size;
+				nowNode.usedCatalogQuota -= toBeDeleteSize;
+			}
+			else {
+				toBeDeleteSize = nowNode.unborn.get(path[i]).usedUnbornQuota;
+			}
+			nowNode.unborn.remove(path[i]);
+		}
+		else {
+			return true;
+		}
+		nowNode = root;
+		if(path.length == 1) {
+			nowNode.usedUnbornQuota -= toBeDeleteSize;
+			return true;
+		}
+		for(i = 1; i < path.length; ++i) {
+			nowNode.usedUnbornQuota -= toBeDeleteSize;
+			nowNode = nowNode.unborn.get(path[i]);
+		}
+		return true;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public boolean setQuota(String[] path, long cata, long unborn) {
+		FileNode nowNode = root;
+		int i;
+		for(i = 1; i < path.length; ++i) {
+			if(nowNode.unborn.containsKey(path[i]) 
+					&& nowNode.unborn.get(path[i]).type == false) {
+				nowNode = nowNode.unborn.get(path[i]);
+			}
+			else {
+				return false;
+			}
+		}
+		if(unborn != 0 && unborn < nowNode.usedUnbornQuota) {
+			return false;
+		}
+		if(cata != 0 && cata < nowNode.usedCatalogQuota) {
+			return false;
+		}
+		nowNode.unbornQuota = unborn;
+		nowNode.catalogQuota = cata;
+		return true;
 	}
-	public ArrayList<String> getOperationList(){
-		return operationList;
+	boolean checkUnbornQuota(FileNode toCheck, long size) {
+		if(toCheck.unbornQuota == 0) {
+			return true;
+		}
+		else if(size <= toCheck.unbornQuota - toCheck.usedUnbornQuota) {
+			return true;
+		}
+		return false;
 	}
-	public ArrayList<String> getResourceKindList(){
-		return resourceKindList;
+	boolean checkQuota(FileNode toCheck, long size) {
+		if(toCheck.catalogQuota == 0) {
+			return true;
+		}
+		else if(size <= toCheck.catalogQuota - toCheck.usedCatalogQuota) {
+			return true;
+		}
+		return false;
 	}
-	public ArrayList<String> getResourceNameList(){
-		return resourceNameList;
+	boolean canReplace(FileNode toCheck, FileNode toBeReplace, long size) {
+		if(toCheck.catalogQuota == 0) {
+			return true;
+		}
+		else if(toCheck.catalogQuota - toCheck.usedCatalogQuota
+				+ toBeReplace.size >= size) {
+			return true;
+		}
+		return false;
 	}
-}
-class RoleRelevance{
-	private String roleName;
-	private ArrayList<String> authorizationObjectName;
-	private ArrayList<String> authorizationObjectNameGroup;
-	RoleRelevance(){
-		authorizationObjectName = new ArrayList<String>();
-		authorizationObjectNameGroup = new ArrayList<String>();
-	}
-	public ArrayList<String> getAuthorizationObjectName(){
-		return authorizationObjectName;
-	}
-	public ArrayList<String> getAuthorizationObjectNameGroup(){
-		return authorizationObjectNameGroup;
-	}
-	public String getRoleName() {
-		return roleName;
-	}
-	public void setRoleName(String name) {
-		this.roleName = name;
+	long isReplace(String[] path) {
+		FileNode nowNode = root;
+		int i;
+		for(i = 1; i < path.length - 1; ++i) {
+			if(!nowNode.unborn.containsKey(path[i])) {
+				return 0;
+			}
+			nowNode = nowNode.unborn.get(path[i]);
+		}
+		if(nowNode.unborn.containsKey(path[i]) && nowNode.unborn.get(path[i]).type) {
+			return nowNode.unborn.get(path[i]).size;
+		}
+		return 0;
 	}
 }
