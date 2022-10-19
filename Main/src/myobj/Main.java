1d3e1ae28a4e999db8d0d590a516f7edb1758c1b
wblm <wblm@LAPTOP-RHSPI2BT.mshome.net>
2022-10-19 22:30:06 +0800

    no message

--- Main/src/myobj/Main.java
+++ Main/src/myobj/Main.java
@@ -3,236 +3,182 @@ import java.util.*;
 /**
  * @author linxingde
  */
+
+
 public class Main {
+	public final static char ISID = '#';
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
-		int t = cin.nextInt();
+		int n = cin.nextInt();
+		int m = cin.nextInt();
 		cin.nextLine();
-		FileRoot system = new FileRoot();
-		while(t-- != 0) {
+		ArrayList<Node> line = new ArrayList<Node>();
+		addNode(line, n, cin);
+		for(int i = 0; i < m; ++i) {
 			String s = cin.nextLine();
 			String[] operator = s.split(" ");
-			if("C".equals(operator[0])) {
-				if(system.createFile(operator[1].split("/"), Long.parseLong(operator[2]))) {
-					System.out.println("Y");
+			if(operator.length == 1) {
+				if(operator[0].charAt(0) == ISID) {
+					ArrayList<Integer> a = idChooser(line, operator[0]);
+					for(int j = 0; j < a.size(); ++j) {
+						if(j == a.size() - 1) {
+							System.out.println(a.get(j));
+						}
+						else {
+							System.out.print(a.get(j) + " ");
+						}
+					}
 				}
 				else {
-					System.out.println("N");
+					ArrayList<Integer> a = labelChooser(line, operator[0].toLowerCase());
+					for(int j = 0; j < a.size(); ++j) {
+						if(j == a.size() - 1) {
+							System.out.println(a.get(j));
+						}
+						else {
+							System.out.print(a.get(j) + " ");
+						}
+					}
 				}
 			}
-			else if("R".equals(operator[0])) {
-				system.removeFile(operator[1].split("/"));
-				System.out.println("Y");
-			}
-			else if("Q".equals(operator[0])) {
-				if(system.setQuota(operator[1].split("/"), Long.parseLong(operator[2]),
-						Long.parseLong(operator[3]))) {
-					System.out.println("Y");
-				}
-				else {
-					System.out.println("N");
+			else {
+				ArrayList<Integer> a = unbornChooser(line, operator);
+				for(int j = 0; j < a.size(); ++j) {
+					if(j == a.size() - 1) {
+						System.out.println(a.get(j));
+					}
+					else {
+						System.out.print(a.get(j) + " ");
+					}
 				}
 			}
 		}
 		cin.close();
-	}	
+	}
+	static class Node{
+		String label;
+		String id;
+		int level;
+		Node(String label, String id, int level){
+			this.label = label;
+			this.id = id;
+			this.level = level;
+		}
+	}
+	static void addNode(ArrayList<Node> line, int n, Scanner cin) {
+		for(int i = 0; i < n; ++i) {
+			String s = cin.nextLine();
+			int level = numOfPoint(s);
+			s = deletePoint(s, 2 * level);
+			String[] operator = s.split(" ");
+			line.add(new Node(operator[0].toLowerCase(), 
+					(operator.length > 1 ? operator[1] : ""), level));
+			
+		}
+	}
+	static ArrayList<Integer> labelChooser(ArrayList<Node> line, String toSearch) {
+		ArrayList<Integer> out = new ArrayList<Integer>();
+		out.add(0);
+		for(int i = 0; i < line.size(); ++i) {
+			if(toSearch.equals(line.get(i).label)) {
+				out.add(i + 1);
+				out.set(0, out.get(0) + 1);
+			}
+		}
+		return out;
+	}
+	static ArrayList<Integer> idChooser(ArrayList<Node> line, String toSearch) {
+		ArrayList<Integer> out = new ArrayList<Integer>();
+		out.add(0);
+		for(int i = 0; i < line.size(); ++i) {
+			if(toSearch.equals(line.get(i).id)) {
+				out.add(i + 1);
+				out.set(0, out.get(0) + 1);
+			}
+		}
+		return out;
+	}
+	static ArrayList<Integer> unbornChooser(ArrayList<Node> line, String[] operator) {
+		ArrayList<Integer> appearLine;
+		ArrayList<Integer> out = new ArrayList<Integer>();
+		out.add(0);
+		//是id
+		if(operator[operator.length - 1].charAt(0) == ISID) {
+			//得到尾元素所在行的集合
+			appearLine = idChooser(line, operator[operator.length - 1]);
+			//对每一个存在尾元素的行进行判断
+			for(int i = 1; i <= appearLine.get(0); ++i) {
+				int toJudgeLine = appearLine.get(i);
+				int nowLine = appearLine.get(i) - 1;
+				int nowLevel = line.get(nowLine).level;
+				int j = operator.length - 2;
+				//对该行进行判断
+				while(j >= 0 && nowLine > 0) {
+					if(line.get(nowLine - 1).level == nowLevel - 1) {
+						if(line.get(nowLine - 1).id.equals(operator[j])
+								|| line.get(nowLine - 1).label.equals(operator[j])) {
+							--j;
+						}
+						--nowLevel;
+					}
+					--nowLine;
+				}
+				if(j < 0) {
+					out.add(toJudgeLine);
+					out.set(0, out.get(0) + 1);
+				}
+			}
+		}
+		//是标签
+		else {
+			for(int i = 0; i < operator.length; ++i) {
+				if(operator[i].charAt(0) != '#') {
+				operator[i] = operator[i].toLowerCase();
+				}
+			}
+			//得到尾元素所在行的集合
+			appearLine = labelChooser(line, operator[operator.length - 1]);
+			//对每一个存在尾元素的行进行判断
+			for(int i = 1; i <= appearLine.get(0); ++i) {
+				int toJudgeLine = appearLine.get(i);
+				int nowLine = appearLine.get(i) - 1;
+				int nowLevel = line.get(nowLine).level;
+				int j = operator.length - 2;
+				//对该行进行判断
+				while(j >= 0 && nowLine > 0) {
+					if(line.get(nowLine - 1).level == nowLevel - 1) {
+						if(line.get(nowLine - 1).label.equals(operator[j])
+								|| line.get(nowLine - 1).id.equals(operator[j])) {
+							--j;
+						}
+						--nowLevel;
+					}
+					--nowLine;
+				}
+				if(j < 0) {
+					out.add(toJudgeLine);
+					out.set(0, out.get(0) + 1);
+				}
+			}
+		}
+		return out;
+	}
+	static int numOfPoint(String s) {
+		int sum = 0;
+		for(int i = 0; i < s.length(); ++i) {
+			if(s.charAt(i) == '.') {
+				++sum;
+			}
+			else {
+				return sum / 2;
+			}
+		}
+		return sum / 2;
+	}
+	static String deletePoint(String s, int begin) {
+		return s.substring(begin);
+	}
+	
 };
 
-class FileNode{
-	boolean type;
-	long size;
-	long catalogQuota, usedCatalogQuota;
-	long unbornQuota, usedUnbornQuota;
-	HashMap<String, FileNode> unborn;
-	FileNode() {
-		this.usedCatalogQuota = 0;
-		this.usedUnbornQuota = 0;
-		this.catalogQuota = 0;
-		this.unbornQuota = 0;
-		this.size = 0;
-		this.type = false;
-		this.unborn = new HashMap<>();
-	}
-	FileNode(long size) {
-		this.usedCatalogQuota = 0;
-		this.usedUnbornQuota = 0;
-		this.catalogQuota = 0;
-		this.unbornQuota = 0;
-		this.type = true;
-		this.size = size;
-		this.unborn = new HashMap<>();
-	}
- }
-class FileRoot{
-	FileNode root;
-	FileRoot(){
-		root = new FileNode();
-	}
-	public boolean createFile(String[] path, long size) {
-		if(path.length == 0) {
-			return false;
-		}
-		FileNode nowNode = root;
-		int i;
-		long beReplaceSize = size - isReplace(path);
-		for(i = 1; i < path.length - 1; ++i) {
-			if(nowNode.unborn.containsKey(path[i])) {
-				if(nowNode.unborn.get(path[i]).type == true) {
-					return false;
-				}
-				else {
-					if(checkUnbornQuota(nowNode, beReplaceSize)){
-						nowNode = nowNode.unborn.get(path[i]);
-					}
-					else {
-						return false;
-					}
-				}
-			}
-			else {
-				if(checkUnbornQuota(nowNode, size)) {
-					nowNode.unborn.put(path[i], new FileNode());
-					nowNode = nowNode.unborn.get(path[i]);
-				}
-				else {
-					return false;
-				}
-			}
-		}
-		if(!nowNode.unborn.containsKey(path[i])) {
-			if(checkQuota(nowNode, size) && checkUnbornQuota(nowNode, size)) {
-				nowNode.unborn.put(path[i], new FileNode(size));
-				nowNode.usedCatalogQuota += size;
-			}
-			else {
-				return false;
-			}
-		}
-		else {
-			if(nowNode.unborn.get(path[i]).type == false) {
-				return false;
-			}
-			else {
-				if(canReplace(nowNode, nowNode.unborn.get(path[i]), size)) {
-					nowNode.usedCatalogQuota += beReplaceSize;
-					nowNode.unborn.get(path[i]).size = size;
-				}
-				else {
-					return false;
-				}
-			}
-		}
-		nowNode = root;
-		for(i = 1; i < path.length; ++i) {
-			nowNode.usedUnbornQuota += beReplaceSize;
-			nowNode = nowNode.unborn.get(path[i]);
-		}
-		return true;
-	}
-	public boolean removeFile(String[] path) {
-		if(path.length == 0) {
-			return true;
-		}
-		FileNode nowNode = root;
-		int i;
-		long toBeDeleteSize = 0;
-		for(i = 1; i < path.length - 1; ++i) {
-			if(nowNode.unborn.containsKey(path[i])) {
-				nowNode = nowNode.unborn.get(path[i]);
-			}
-			else {
-				return true;
-			}
-		}
-		if(nowNode.unborn == null) {
-			return true;
-		}
-		if(nowNode.unborn.containsKey(path[i])) {
-			if(nowNode.unborn.get(path[i]).type) {
-				toBeDeleteSize = nowNode.unborn.get(path[i]).size;
-				nowNode.usedCatalogQuota -= toBeDeleteSize;
-			}
-			else {
-				toBeDeleteSize = nowNode.unborn.get(path[i]).usedUnbornQuota;
-			}
-			nowNode.unborn.remove(path[i]);
-		}
-		else {
-			return true;
-		}
-		nowNode = root;
-		if(path.length == 1) {
-			nowNode.usedUnbornQuota -= toBeDeleteSize;
-			return true;
-		}
-		for(i = 1; i < path.length; ++i) {
-			nowNode.usedUnbornQuota -= toBeDeleteSize;
-			nowNode = nowNode.unborn.get(path[i]);
-		}
-		return true;
-	}
-	public boolean setQuota(String[] path, long cata, long unborn) {
-		FileNode nowNode = root;
-		int i;
-		for(i = 1; i < path.length; ++i) {
-			if(nowNode.unborn.containsKey(path[i]) 
-					&& nowNode.unborn.get(path[i]).type == false) {
-				nowNode = nowNode.unborn.get(path[i]);
-			}
-			else {
-				return false;
-			}
-		}
-		if(unborn != 0 && unborn < nowNode.usedUnbornQuota) {
-			return false;
-		}
-		if(cata != 0 && cata < nowNode.usedCatalogQuota) {
-			return false;
-		}
-		nowNode.unbornQuota = unborn;
-		nowNode.catalogQuota = cata;
-		return true;
-	}
-	boolean checkUnbornQuota(FileNode toCheck, long size) {
-		if(toCheck.unbornQuota == 0) {
-			return true;
-		}
-		else if(size <= toCheck.unbornQuota - toCheck.usedUnbornQuota) {
-			return true;
-		}
-		return false;
-	}
-	boolean checkQuota(FileNode toCheck, long size) {
-		if(toCheck.catalogQuota == 0) {
-			return true;
-		}
-		else if(size <= toCheck.catalogQuota - toCheck.usedCatalogQuota) {
-			return true;
-		}
-		return false;
-	}
-	boolean canReplace(FileNode toCheck, FileNode toBeReplace, long size) {
-		if(toCheck.catalogQuota == 0) {
-			return true;
-		}
-		else if(toCheck.catalogQuota - toCheck.usedCatalogQuota
-				+ toBeReplace.size >= size) {
-			return true;
-		}
-		return false;
-	}
-	long isReplace(String[] path) {
-		FileNode nowNode = root;
-		int i;
-		for(i = 1; i < path.length - 1; ++i) {
-			if(!nowNode.unborn.containsKey(path[i])) {
-				return 0;
-			}
-			nowNode = nowNode.unborn.get(path[i]);
-		}
-		if(nowNode.unborn.containsKey(path[i]) && nowNode.unborn.get(path[i]).type) {
-			return nowNode.unborn.get(path[i]).size;
-		}
-		return 0;
-	}
-}
+
